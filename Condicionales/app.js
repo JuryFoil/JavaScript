c22edeef2c9f0ac46dc71a808e5d49a64564ff57
JuryFoil <mgarcia14@ucol.mx>
2023-10-25 19:05:04 -0600

    Actualizacion

--- Condicionales/app.js
+++ Condicionales/app.js
@@ -14,31 +14,76 @@
 //         document.write("eres mayor de edad");
 //     }else{
 //         Document.write("Eres menor de edad");
+// //     }
+// // }else {
+// //     document.write("Edad no valida");
+// // }
+
+
+// let dia=prompt("Introduce un dia").toLowerCase();
+
+// switch (dia) {
+//     case "lunes":
+//         console.log("El dia que seleccionaste es lunes");
+//         break;
+//     case "martes":
+//         console.log("el dia que seleccionaste es martes");
+//         break;
+//     case "miercoles":
+//         console.log("El dia que seleccionaste es miercoles");
+//         break;
+//     case "jueves":
+//         console.log("el dia que seleccionaste es jueves");
+//         break;
+//     case "viernes":
+//         console.log("El dia que seleccionaste es viernes");
+//         break;
+//     default:
+//         console.log("Este no es un dia")
+//         break;
+// }
+// //Operador ternario
+// let numero1= 15;
+// let numero2= 20;
+
+// let valor=(numero1 > numero2) ? "el numero1 es mayor":"El numero2 es mayor"
+
+
+//Mayor o menor que
+// let n1=prompt("Introduce un numero");
+// let n2=prompt("Introduce otro numero");
+
+// if (n1<n2) {
+//     document.write("El numero "+n1+" es menor que el numero "+n2);
+    
+// } else {
+//     if (n1>n2) {
+//         document.write("El numero "+n1+" es mayor que el numero "+n2);
+        
+//     } else {
+//         document.write("Los numeros introducidos son iguales")
 //     }
-// }else {
-//     document.write("Edad no valida");
+    
 // }
 
+// //Par o impar
+// let nu=prompt("Introduce un numero");
+// // let res= nu%2
+// if (nu % 2==0) {
+//     document.write("El numero es par");
+// } else {
+//     document.write("El numero es impar");
+// }
 
-let dia=prompt("Introduce un dia").toLowerCase();
+//Pedir dos numeros enteros y devolver el cociente, es decir, el resultado de 
+//Dividir el numero 1 entre el numero 2, pero si uno de los numero es 0, mandar mensaje de error
 
-switch (dia) {
-    case "lunes":
-        console.log("El dia que seleccionaste es lunes");
-        break;
-    case "martes":
-        console.log("el dia que seleccionaste es martes");
-        break;
-    case "miercoles":
-        console.log("El dia que seleccionaste es miercoles");
-        break;
-    case "jueves":
-        console.log("el dia que seleccionaste es jueves");
-        break;
-    case "viernes":
-        console.log("El dia que seleccionaste es viernes");
-        break;
-    default:
-        console.log("Este no es un dia")
-        break;
+let nu1=prompt("Introduce un numero entero");
+let nu2=prompt("Introduce otro numero entero");
+
+if ((nu1)||(nu2)==0) {
+    alert("Error, ambos numeros deben ser mayor a 0");
+} else {
+    let div=nu1/nu2;
+    document.write("El resultado de la operacion es " + div);
 }
